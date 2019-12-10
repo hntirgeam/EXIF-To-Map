d2190a9f0e09a207a77b758f177f44a778997840
fujig <kmacroviperaz@gmail.com>
2019-12-11 02:31:39 +0300

    .h to .hpp

---
+++ mainprocess.hpp
@@ -0,0 +1,24 @@
+#ifndef MAINPROCESS_H
+#define MAINPROCESS_H
+
+#include <iostream>
+#include <fstream>
+#include <algorithm>
+#include <vector>
+#include <string>
+#include <experimental/filesystem>
+
+
+class MainProcess
+{
+
+public:
+    
+    std::vector<std::string> getPath(const std::string &dirPath);
+
+    void retrieveGpsCoordinate(std::string file_name, double &lat, double &lon, double &alt);
+
+    void sendGpsToTxt(const std::vector<std::string> &file_name);
+};
+
+#endif
