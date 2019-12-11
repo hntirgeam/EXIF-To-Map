c00160690d6edcb34967f9568e9d4921d50d60ed
fujig <kmacroviperaz@gmail.com>
2019-12-11 23:11:54 +0300

    .h to .hpp

--- exif.cpp
+++ exif.cpp
@@ -23,7 +23,7 @@
   NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE,
   EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
 */
-#include "exif.h"
+#include "exif.hpp"
 
 #include <algorithm>
 #include <cstdint>
