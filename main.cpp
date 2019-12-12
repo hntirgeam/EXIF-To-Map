9c8e9aef8138f3c469082a5fce7a9cda6693249e
fujig <kmacroviperaz@gmail.com>
2019-12-12 03:08:33 +0300

    added while

--- main.cpp
+++ main.cpp
@@ -1,27 +1,38 @@
-#include "mainprocess.h"
+#include "mainprocess.hpp"
 
 #include <iostream>
 #include <vector>
+#include <string>
 
-int main(int argc, char *argv[])
+int main()
 {
+    namespace fs = std::experimental::filesystem;
+
+    std::string dir = {};    
     
-
-    std::string dir = {};
-
-    if (argc < 2) 
+    while(true)
     {
+
         std::cout << "Pass me folder/directory with images pls: ";
-        std::cin >> dir;
-    }
-    else
-    {
-        dir = argv[1];
+        std::getline(std::cin, dir);
+
+        if(!fs::is_directory(dir))
+        {
+            std::cerr << "Not a directory\n";
+            continue;
+        }
+
+        if(fs::is_empty(dir))
+        {
+            std::cerr << "Directory is empty\n";
+            continue;
+        }
+        break;
     }
 
     MainProcess map;
 
-    std::vector urls = map.getPath(dir);
+    std::vector<std::string> urls = map.getPath(dir);
     map.sendGpsToTxt(urls);
 
     return 0;
